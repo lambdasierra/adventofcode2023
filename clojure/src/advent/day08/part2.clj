d5d79e96155bc26909c0de5d09c3371fc7068e67
Alessandra Sierra <105090644+lambdasierra@users.noreply.github.com>
2023-12-10 16:21:08 -0500

    Day 8 part 2 complete in Clojure
    Based on Reddit comments saying that the only tractable solution
    solution is to use the least common multiple of the individual path
    lengths.

--- clojure/src/advent/day08/part2.clj
+++ clojure/src/advent/day08/part2.clj
@@ -8,17 +8,30 @@
 (defn end-label? [label]
   (string/ends-with? label "Z"))
 
-(defn count-steps [instructions index]
-  (loop [positions (filterv start-label? (keys index))
+(defn count-steps [instructions index start-label]
+  (loop [position start-label
          instructions (cycle instructions)
          steps 0]
-    (if (every? end-label? positions)
+    (if (end-label? position)
       steps
-      (let [instruction (first instructions)]
-        (recur (mapv #(get-in index [% instruction]) positions)
-               (rest instructions)
-               (inc steps))))))
+      (recur (get-in index [position (first instructions)])
+             (rest instructions)
+             (inc steps)))))
+
+(defn gcd
+  "Greatest common divisor"
+  [a b]
+  (if (zero? b)
+    a
+    (recur b (mod a b))))
+
+(defn lcm
+  "Least common multiple"
+  [a b]
+  (* (abs a) (/ (abs b) (gcd a b))))
 
 (defn run [input-path]
-  (let [{:keys [instructions network]} (part1/load-input input-path)]
-    (count-steps instructions (part1/index-network network))))
+  (let [{:keys [instructions network]} (part1/load-input input-path)
+        index (part1/index-network network)
+        starting-nodes (filter start-label? (keys index))]
+    (reduce lcm (map #(count-steps instructions index %) starting-nodes))))
--- clojure/src/advent/day08/part2_repl.clj
+++ clojure/src/advent/day08/part2_repl.clj
@@ -1,7 +1,44 @@
 (in-ns 'advent.day08.part2)
 
 (run "../input/day08/sample3.txt")
-;; => 6
+
+(input-nodes (part1/load-input "../input/day08/sample3.txt"))
+;; => ["11A" "22A"]
+
+(input-nodes (part1/load-input "../input/day08/input.txt"))
+;; => ["DFA" "BLA" "TGA" "AAA" "PQA" "CQA"]
+
+(def the-input (part1/load-input "../input/day08/input.txt"))
+(def the-instructions (:instructions the-input))
+(def the-network (:network the-input))
+(def the-index (part1/index-network the-network))
+
+(count-steps the-instructions the-index "DFA")
+;; => 18157
+
+(count-steps the-instructions the-index "BLA")
+;; => 14363
+
+(count-steps the-instructions the-index "TGA")
+;; => 19783
+
+(count-steps the-instructions the-index "AAA")
+;; => 15989
+
+(count-steps the-instructions the-index "PQA")
+;; => 19241
+
+(count-steps the-instructions the-index "CQA")
+;; => 12737
+
+(gcd 1071 462)
+;; => 21
+
+(lcm 21 6)
+;; => 42
+
+(reduce lcm [18157 14363 19783 15989 19241 12737])
+;; => 13830919117339
 
 (run "../input/day08/input.txt")
-;; takes a long time...
+;; => 13830919117339
