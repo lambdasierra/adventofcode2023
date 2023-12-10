9908099494f60a2d56c5b66bca9410684811bd17
Alessandra Sierra <105090644+lambdasierra@users.noreply.github.com>
2023-12-09 20:37:21 -0500

    Day 6 part 2: complete in Clojure
    Still brute-force but it still works
    in a few seconds.

---
+++ clojure/src/advent/day06/part2.clj
@@ -0,0 +1,15 @@
+(ns advent.day06.part1
+  (:require [advent.day06.part1 :as part1]
+            [clojure.java.io :as io]))
+
+(defn parse-digits [string]
+  (parse-long (apply str (re-seq #"\d" string))))
+
+(defn load-input [input-path]
+  (with-open [rdr (io/reader input-path)]
+    (let [[times-line distance-line] (line-seq rdr)]
+      {:time (parse-digits times-line)
+       :distance (parse-digits distance-line)})))
+
+(defn run [input-path]
+  (count (part1/winning-options (load-input input-path))))
---
+++ clojure/src/advent/day06/part2_repl.clj
@@ -0,0 +1,7 @@
+(in-ns 'advent.day06.part2)
+
+(run "../input/day06/sample.txt")
+;; => 71503
+
+(run "../input/day06/input.txt")
+;; => 21039729
